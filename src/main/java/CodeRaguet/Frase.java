00d72c85d130cdc9af3a34af63b9d9d614e70014
Code Raguet <ignacio.code@gmail.com>
2016-10-20 21:11:09 -0300

    Add Frase class

---
+++ src/main/java/CodeRaguet/Frase.java
@@ -0,0 +1,16 @@
+package CodeRaguet;
+
+
+public class Frase {
+
+    private final String frase;
+
+    public Frase(String frase) {
+        this.frase = frase;
+    }
+
+    @Override
+    public String toString() {
+        return frase;
+    }
+}
--- src/main/java/CodeRaguet/Main.java
+++ src/main/java/CodeRaguet/Main.java
@@ -4,7 +4,8 @@ package CodeRaguet;
 public class Main {
 
     public static void main(String... args) {
-        System.out.println("Desafiando a la luz, tuve claridad. (La Renga)");
+        Frase frase = new Frase("Desafiando a la luz, tuve claridad. (La Renga)");
+        System.out.println(frase);
     }
 
 }
