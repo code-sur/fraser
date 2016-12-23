03e59702379e0febd1a9c04d1461e079f60fe264
Code Raguet <ignacio.code@gmail.com>
2016-12-23 11:25:14 -0300

    Refactor getGmailLabels

--- src/test/java/CodeRaguet/fraser/IT/FraserEndToEndIT.java
+++ src/test/java/CodeRaguet/fraser/IT/FraserEndToEndIT.java
@@ -7,13 +7,13 @@ import java.io.IOException;
 
 public class FraserEndToEndIT {
 
-    private static final String LAST_LABEL = "IMPORTANT";
-
     private ApplicationRunner application = new ApplicationRunner();
 
     @Test
     public void getGmailLabels() throws IOException, InterruptedException {
         application.getLabels();
-        application.showsLabelsContainig(LAST_LABEL);
+
+        String lastLabel = "IMPORTANT";
+        application.showsLabelsContainig(lastLabel);
     }
 }
