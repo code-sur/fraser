4a166e34edabb7f50c77128914851f6f41863ba6
Code Raguet <ignacio.code@gmail.com>
2016-12-24 13:44:08 -0300

    Extract CLIENT_SECRET to gmail.porperties

--- src/test/java/CodeRaguet/fraser/IT/FraserIT.java
+++ src/test/java/CodeRaguet/fraser/IT/FraserIT.java
@@ -20,7 +20,7 @@ public class FraserIT {
         inStream.close();
 
         fraser.setRefreshToken(gmailProperties.getProperty("REFRESH_TOKEN"));
-        fraser.setClientSecret("{\"installed\":{\"client_id\":\"379299895047-2lj1s61cu20qf4dnr5os33nsvft3uumb.apps.googleusercontent.com\",\"project_id\":\"weighty-psyche-153000\",\"auth_uri\":\"https://accounts.google.com/o/oauth2/auth\",\"token_uri\":\"https://accounts.google.com/o/oauth2/token\",\"auth_provider_x509_cert_url\":\"https://www.googleapis.com/oauth2/v1/certs\",\"client_secret\":\"gpS2Fvi62D0zvUeOPufeQUB7\",\"redirect_uris\":[\"urn:ietf:wg:oauth:2.0:oob\",\"http://localhost\"]}}");
+        fraser.setClientSecret(gmailProperties.getProperty("CLIENT_SECRET"));
 
         fraser.run();
 
