faf81f282b807394ae2afce66eabe95e665b37d5
Code Raguet <ignacio.code@gmail.com>
2016-12-25 21:04:03 -0300

    Inline runFraser()

--- src/test/java/CodeRaguet/fraser/IT/FraserRunner.java
+++ src/test/java/CodeRaguet/fraser/IT/FraserRunner.java
@@ -14,18 +14,14 @@ class FraserRunner {
     private final ProcessBuilder processBuilder = new ProcessBuilder("bash", "fraser.sh");
     private final Map<String, String> env = processBuilder.environment();
 
-    private String runFraser() throws IOException, InterruptedException {
+    void run() throws IOException, InterruptedException {
         processBuilder.redirectErrorStream(true);
         Process p = processBuilder.start();
         p.waitFor();
 
         InputStreamReader inputStreamReader = new InputStreamReader(p.getInputStream());
         BufferedReader br = new BufferedReader(inputStreamReader);
-        return br.readLine();
-    }
-
-    void run() throws IOException, InterruptedException {
-        stdout = runFraser();
+        stdout = br.readLine();
     }
 
     void shows(String label) {
