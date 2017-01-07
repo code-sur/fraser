f6141484d74b58852963ad22c2b4d26164e0edde
Code Raguet <ignacio.code@gmail.com>
2017-01-06 21:32:55 -0300

    Add acceptance test for publishing first frase

--- src/test/java/CodeRaguet/fraser/IT/FraserIT.java
+++ src/test/java/CodeRaguet/fraser/IT/FraserIT.java
@@ -2,6 +2,7 @@ package CodeRaguet.fraser.IT;
 
 
 import CodeRaguet.fraser.Frase;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,14 +11,24 @@ public class FraserIT extends IntegrationTest {
 
     private static final Frase FIRST_FRASE = new Frase("El infierno es el olvido");
     private final FraserRunner fraser = new FraserRunner();
+    private FraserPublicationsServer frasePublicationsServer;
+
+    @Before
+    public void setUpFraser() {
+        fraser.with(testENV);
+    }
+
+    @Before
+    public void setUpPublicationsServer() {
+        frasePublicationsServer = new FraserPublicationsServer(testENV);
+        frasePublicationsServer.deleteFrases();
+    }
 
     @Test
-    public void fraserRun() throws IOException, InterruptedException {
-        fraser.with(testENV);
-
+    public void shouldPublishFirstFrase() throws IOException, InterruptedException {
         fraser.run();
 
-        fraser.posterShows(FIRST_FRASE);
+        frasePublicationsServer.hasRecived(FIRST_FRASE);
     }
 
 }
