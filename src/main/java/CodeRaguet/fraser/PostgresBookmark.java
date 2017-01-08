7e70029ca88add08ebca9356d605906ef16be7e9
Code Raguet <ignacio.code@gmail.com>
2017-01-08 10:33:37 -0300

    Add PostgresBookmark

---
+++ src/main/java/CodeRaguet/fraser/PostgresBookmark.java
@@ -0,0 +1,31 @@
+package CodeRaguet.fraser;
+
+import java.sql.*;
+
+public class PostgresBookmark implements Bookmark {
+
+    private static final String FRASE_TEXT_COLUMN = "FRASE";
+    private static final String BOOKMARK_TABLE = "BOOKMARK";
+
+    @Override
+    public Frase isAt() {
+
+        ResultSet resultSet;
+        String fraseText;
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/fraser", "fraser", "fraser");
+            Statement statement = connection.createStatement();
+            resultSet = statement.executeQuery(String.format("SELECT %s from %s", FRASE_TEXT_COLUMN, BOOKMARK_TABLE));
+            resultSet.next();
+            fraseText = resultSet.getString(FRASE_TEXT_COLUMN);
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return new Frase(fraseText);
+    }
+
+}
---
+++ src/test/java/CodeRaguet/fraser/IT/PostgresBookmarkTest.java
@@ -0,0 +1,41 @@
+package CodeRaguet.fraser.IT;
+
+import CodeRaguet.fraser.Bookmark;
+import CodeRaguet.fraser.Frase;
+import CodeRaguet.fraser.PostgresBookmark;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PostgresBookmarkTest {
+
+    @Before
+    public void clearBookmark() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/fraser", "fraser", "fraser");
+        String sql = "TRUNCATE TABLE bookmark";
+        Statement stmt = connection.createStatement();
+        stmt.execute(sql);
+        stmt.close();
+        connection.close();
+    }
+
+    @Test
+    public void test() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/fraser", "fraser", "fraser");
+        String sql = "INSERT INTO BOOKMARK (FRASE, FECHA) VALUES ('First frase', DATE '2016-12-28')";
+        Statement stmt = connection.createStatement();
+        stmt.execute(sql);
+        stmt.close();
+        connection.close();
+
+        Bookmark bookmark = new PostgresBookmark();
+        assertThat(bookmark.isAt()).isEqualTo(new Frase("First frase"));
+    }
+
+}
