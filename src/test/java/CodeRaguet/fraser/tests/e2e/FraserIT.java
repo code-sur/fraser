cd473f2d60bad944a3f52b97a2ea0c1ee2af00f1
Ignacio Raguet <ignacio.raguet@grupoesfera.com.ar>
2017-01-09 14:30:34 -0300

    Make FraserIT a DatabaseTest

--- src/test/java/CodeRaguet/fraser/tests/e2e/FraserIT.java
+++ src/test/java/CodeRaguet/fraser/tests/e2e/FraserIT.java
@@ -1,40 +1,22 @@
 package CodeRaguet.fraser.tests.e2e;
 
 
-import CodeRaguet.fraser.ENV;
 import CodeRaguet.fraser.model.Frase;
 import CodeRaguet.fraser.model.NoBookmarkException;
-import CodeRaguet.fraser.tests.HerokuDBURLParser;
-import CodeRaguet.fraser.tests.ENVTest;
-import org.junit.AfterClass;
+import CodeRaguet.fraser.tests.DatabaseTest;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class FraserIT extends ENVTest {
+public class FraserIT extends DatabaseTest {
 
     private static final Frase FIRST_FRASE = new Frase("El infierno es el olvido");
     private static final Frase SECOND_FRASE = new Frase("No te llevas nada");
-    private static Connection connection;
     private final FraserRunner fraser = new FraserRunner();
     private FraserPublicationsServer frasePublicationsServer;
     private PostgresBookmarkServer bookmarServer;
-
-    @BeforeClass
-    public static void createConnection() throws SQLException {
-        String jdbcURL = HerokuDBURLParser.parse(testENV.getProperty(ENV.DATABASE_URL.name()));
-        connection = DriverManager.getConnection(jdbcURL);
-    }
-
-    @AfterClass
-    public static void closeConnection() throws SQLException {
-        connection.close();
-    }
 
     @Before
     public void setUpFraser() {
