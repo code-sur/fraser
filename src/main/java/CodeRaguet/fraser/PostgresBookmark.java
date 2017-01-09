67efeaa6735ae54d2e1b1546eb49a8fff968bc03
Code Raguet <ignacio.code@gmail.com>
2017-01-08 21:24:28 -0300

    Extract DB_URL, DB_USER, DB_PASSWORD

--- src/main/java/CodeRaguet/fraser/PostgresBookmark.java
+++ src/main/java/CodeRaguet/fraser/PostgresBookmark.java
@@ -10,6 +10,9 @@ public class PostgresBookmark implements Bookmark {
 
     private static final String FRASE_TEXT_COLUMN = "FRASE";
     public static final String BOOKMARK_TABLE = "BOOKMARK";
+    private static final String DB_URL = "jdbc:postgresql://localhost:5432/fraser";
+    private static final String DB_USER = "fraser";
+    private static final String DB_PASSWORD = "fraser";
 
     @Override
     public Frase isAt() throws NoBookmarkException {
@@ -17,7 +20,7 @@ public class PostgresBookmark implements Bookmark {
         ResultSet resultSet;
         String fraseText;
         try {
-            Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/fraser", "fraser", "fraser");
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
             Statement statement = connection.createStatement();
 
             resultSet = statement.executeQuery(String.format("SELECT %s from %s", FRASE_TEXT_COLUMN, BOOKMARK_TABLE));
@@ -40,7 +43,7 @@ public class PostgresBookmark implements Bookmark {
     @Override public void setAt(Frase frase) {
         Connection connection;
         try {
-            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/fraser", "fraser", "fraser");
+            connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
             String sql = String.format("UPDATE BOOKMARK SET FRASE = '%s'", frase);
             Statement stmt = connection.createStatement();
             stmt.execute(sql);
