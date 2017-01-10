2093094e20c210ae13716769d1dcc75f80fd5f99
Ignacio Raguet <ignacio.raguet@grupoesfera.com.ar>
2017-01-10 17:42:01 -0300

    Add Migrations class

---
+++ src/main/java/CodeRaguet/fraser/Migrations.java
@@ -0,0 +1,20 @@
+package CodeRaguet.fraser;
+
+import com.heroku.sdk.jdbc.DatabaseUrl;
+import org.flywaydb.core.Flyway;
+
+public class Migrations {
+
+    public static void main(String[] args) throws Exception {
+        Flyway flyway = new Flyway();
+
+        DatabaseUrl dataBaseUrl = DatabaseUrl.extract();
+        String jdbcUrl = dataBaseUrl.jdbcUrl();
+        String user = dataBaseUrl.username();
+        String password = dataBaseUrl.password();
+        flyway.setDataSource(jdbcUrl, user, password);
+
+        flyway.migrate();
+    }
+
+}
