cc346af4295c61238ea0ae71677abc60bbcd3de1
Code Raguet <ignacio.raguet@grupoesfera.com.ar>
2017-02-08 18:31:18 -0300

    Refactor GmailMessageTranslator.java

--- src/main/java/CodeRaguet/fraser/gmail/GmailMessageTranslator.java
+++ src/main/java/CodeRaguet/fraser/gmail/GmailMessageTranslator.java
@@ -1,14 +1,32 @@
 package CodeRaguet.fraser.gmail;
 
 import com.google.api.services.gmail.model.Message;
+import com.google.api.services.gmail.model.MessagePart;
 import org.jsoup.Jsoup;
 
 import java.util.Base64;
 
 public class GmailMessageTranslator {
 
-    public CodeRaguet.fraser.model.Message translate(Message gmailMessage) {
-        String htmlMessage = new String(Base64.getUrlDecoder().decode(gmailMessage.getPayload().getParts().get(1).getBody().getData()));
-        return new CodeRaguet.fraser.model.Message(Jsoup.parse(htmlMessage).text());
+    public CodeRaguet.fraser.model.Message translate(Message message) {
+        return new CodeRaguet.fraser.model.Message(parseHTML(decodeURLBase64(getHTMLMessage(message))));
     }
+
+    private String parseHTML(String html) {
+        return Jsoup.parse(html).text();
+    }
+
+    private String getHTMLMessage(Message message) {
+        return getHTMLMessagePart(message).getBody().getData();
+    }
+
+    private String decodeURLBase64(String URLBase64encoded) {
+        return new String(Base64.getUrlDecoder().decode(URLBase64encoded));
+    }
+
+    private MessagePart getHTMLMessagePart(Message message) {
+        return message.getPayload().getParts().get(1);
+    }
+
+
 }
