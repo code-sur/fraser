30006b808d90819c1855e8338b9c0d9f6d81d9f2
Code Raguet <ignacio.raguet@grupoesfera.com.ar>
2017-02-08 14:59:23 -0300

    Refactor GmailServiceIT.java

--- src/test/java/CodeRaguet/fraser/tests/integration/GmailServiceIT.java
+++ src/test/java/CodeRaguet/fraser/tests/integration/GmailServiceIT.java
@@ -8,21 +8,37 @@ import CodeRaguet.fraser.tests.tools.ENVTest;
 import CodeRaguet.fraser.tests.tools.fixtures.GmailMessages;
 import com.google.api.services.gmail.model.Message;
 import com.google.api.services.gmail.model.Thread;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class GmailServiceIT extends ENVTest {
 
-    @Test
-    public void getFirstMessageOfThread() {
+    private GmailService gmailService;
+
+    @Before
+    public void setUpGmailService() {
         String refreshToken = testENV.getProperty(ENV.GMAIL_REFRESH_TOKEN.name());
         String clientSecret = testENV.getProperty(ENV.GMAIL_CLIENT_SECRET.name());
-        GmailService gmailService = new GmailService(refreshToken, clientSecret, new GmailFilterTranslator());
+        gmailService = new GmailService(refreshToken, clientSecret, new GmailFilterTranslator());
+    }
+
+    @Test
+    public void getFirstMessageOfThreadWithSnippet() {
         Thread thread = gmailService.getThreadsFilteredBy(new MessageFilter()).get(0);
 
         Message gmailMessage = gmailService.getFirstMessageOf(thread);
 
         assertThat(gmailMessage.getSnippet()).isEqualTo(GmailMessages.firstGmailMessage().getSnippet());
+    }
+
+    @Test
+    public void getFirstMessageOfThreadWithPayload() {
+        Thread thread = gmailService.getThreadsFilteredBy(new MessageFilter()).get(0);
+
+        Message gmailMessage = gmailService.getFirstMessageOf(thread);
+
+        assertThat(gmailMessage.getPayload()).isEqualTo(GmailMessages.firstGmailMessage().getPayload());
     }
 }
