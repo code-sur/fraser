703e92e317ac0d0e2597e4e872f2b17756dba90d
Code Raguet <ignacio.code@gmail.com>
2017-02-11 09:18:53 -0300

    Rename getHTMLMessage to pickHTMLDataFrom

--- src/main/java/CodeRaguet/fraser/gmail/GmailMessageTranslator.java
+++ src/main/java/CodeRaguet/fraser/gmail/GmailMessageTranslator.java
@@ -9,22 +9,22 @@ import java.util.Base64;
 public class GmailMessageTranslator {
 
     public CodeRaguet.fraser.model.Message translate(Message message) {
-        return new CodeRaguet.fraser.model.Message(parseHTML(decodeURLBase64(getHTMLMessage(message))));
+        return new CodeRaguet.fraser.model.Message(parseHTML(decodeURLBase64(pickHTMLDataFrom(message))));
     }
 
     private String parseHTML(String html) {
         return Jsoup.parse(html).text();
     }
 
-    private String getHTMLMessage(Message message) {
-        return getHTMLMessagePart(message).getBody().getData();
+    private String pickHTMLDataFrom(Message message) {
+        return pickHTMLMessagePartFrom(message).getBody().getData();
     }
 
     private String decodeURLBase64(String URLBase64encoded) {
         return new String(Base64.getUrlDecoder().decode(URLBase64encoded));
     }
 
-    private MessagePart getHTMLMessagePart(Message message) {
+    private MessagePart pickHTMLMessagePartFrom(Message message) {
         return message.getPayload().getParts().get(1);
     }
 
